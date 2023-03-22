61c81693b8795036b3f919f1ba2059d42105f8f3
lawff <evilllawliet@gmail.com>
2023-03-22 18:27:17 +0800

    feat: add makefile

---
+++ cmd/miniblog/main.go
@@ -0,0 +1,12 @@
+// Copyright 2023 lawff. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+// Go 程序的默认入口函数(主函数).
+func main() {
+	fmt.Println("Hello MiniBlo33!")
+}

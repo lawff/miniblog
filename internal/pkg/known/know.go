d455938483ed30fa84a06d27dba9fbac90ba05fb
lawff <evilllawliet@gmail.com>
2023-03-23 23:19:23 +0800

    feat: add middleware

---
+++ internal/pkg/known/know.go
@@ -0,0 +1,10 @@
+// Copyright 2023 lawff. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package known
+
+const (
+	// XRequestIDKey 用来定义 Gin 上下文中的键，代表请求的 uuid.
+	XRequestIDKey = "X-Request-ID"
+)

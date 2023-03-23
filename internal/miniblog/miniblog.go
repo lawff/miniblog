b5482b62218f602f0c8bb132d7c0894697cd879b
lawff <evilllawliet@gmail.com>
2023-03-23 13:41:42 +0800

    feat: log

--- internal/miniblog/miniblog.go
+++ internal/miniblog/miniblog.go
@@ -5,10 +5,15 @@
 package miniblog
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/lawff/miniblog/internal/pkg/log"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // NewMiniBlogCommand 创建一个 *cobra.Command 对象. 之后，可以使用 Command 对象的 Execute 方法来启动应用程序.
 func NewMiniBlogCommand() *cobra.Command {
@@ -26,6 +31,11 @@ Find more miniblog information at: https://github.com/lawff/miniblog#readme`,
 		SilenceUsage: true,
 		// 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
 		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// 初始化日志
+			log.Init(logOptions())
+			defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
+
 			return run()
 		},
 		// 这里设置命令运行时，不需要指定命令行参数
@@ -40,11 +50,26 @@ Find more miniblog information at: https://github.com/lawff/miniblog#readme`,
 		},
 	}
 
+	// 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
+	cobra.OnInitialize(initConfig)
+
+	// 在这里您将定义标志和配置设置。
+
+	// Cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the miniblog configuration file. Empty string for no configuration file.")
+
+	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
+	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 	return cmd
 }
 
 // run 函数是实际的业务代码入口函数.
 func run() error {
-	fmt.Println("Hello MiniBlog!")
+	// 打印所有的配置项及其值
+	settings, _ := json.Marshal(viper.AllSettings())
+	log.Infow(string(settings))
+	// 打印 db -> username 配置项的值
+	log.Infow(viper.GetString("db.username"))
 	return nil
 }
